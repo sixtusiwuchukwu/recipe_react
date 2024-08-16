4ce893fa8413d93fafff66994e054ef71c9503c9
sixtus iwuchukwu <sixtusiwuchukwu21@gmail.com>
2024-08-16 11:05:26 +0100

    integrated the update recipe endpoint

--- src/component/recipe/recipeForm.js
+++ src/component/recipe/recipeForm.js
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import ReactQuill from 'react-quill'; // Assuming React Quill is used as the text editor
-import 'react-quill/dist/quill.snow.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import ReactQuill from "react-quill"; // Assuming React Quill is used as the text editor
+import "react-quill/dist/quill.snow.css";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
-
-
-
+import axios from "axios";
 
 const CreateRecipe = () => {
   const location = useLocation();
-  const navigate = useNavigate()
-  const { recipeData } = location.state || {}; 
-  const [recipeName, setRecipeName] = useState(recipeData?.title || '');
-  const [ingredients, setIngredients] = useState(recipeData?.ingredients || ['']);
-  const [instructions, setInstructions] = useState(recipeData?.instructions || '');
-  const [image, setImage] = useState(null);
+  const navigate = useNavigate();
+  const { recipeData } = location.state || {};
+  const [recipeName, setRecipeName] = useState(recipeData?.title || "");
+  const [ingredients, setIngredients] = useState(
+    recipeData?.ingredients || [""]
+  );
+  const [instructions, setInstructions] = useState(
+    recipeData?.instructions || ""
+  );
+  const [image, setImage] = useState(recipeData?.image || null);
   const [imagePreview, setImagePreview] = useState(recipeData?.image || null);
+  const [loading, setLoading] = useState(false);
 
   const handleIngredientChange = (index, value) => {
     if (value.trim() !== "") {
       const updatedIngredients = [...ingredients];
-    updatedIngredients[index] = value.trim();
-    setIngredients(updatedIngredients);
+      updatedIngredients[index] = value.trim();
+      setIngredients(updatedIngredients);
     }
-    
   };
 
   const handleImageChange = (e) => {
@@ -35,12 +37,11 @@ const CreateRecipe = () => {
         setImagePreview(reader.result); // Preview the image using Base64 string
       };
       reader.readAsDataURL(file); // Convert image to Base64
-      
     }
   };
 
   const addIngredientField = () => {
-    setIngredients([...ingredients, '']);
+    setIngredients([...ingredients, ""]);
   };
 
   const removeIngredientField = (index) => {
@@ -51,41 +52,78 @@ const CreateRecipe = () => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
-    console.log("new",{
+    console.log("new", {
       name: recipeName,
       ingredients,
       instructions,
-      image
+      image,
     });
-  }; 
-  const handleUpdate = (e) => {
+  };
+  const handleUpdate = async (e) => {
     e.preventDefault();
-    // Handle form submission logic here
-    console.log("update",{
-      name: recipeName,
-      ingredients,
-      instructions,
-      image
-    });
+    setLoading(true);
+    const response = await axios.put(
+      `https://recipe-server-2fbx.onrender.com/api/recipes/${recipeData._id}`,
+      {
+        name: recipeName,
+        ingredients,
+        instructions,
+        image,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    setRecipeName(response?.data?.title);
+    setIngredients(response.data?.ingredients);
+    setInstructions(response.data?.instructions);
+    setImage(response.data.image);
+    setImagePreview(response.data.image);
+    setLoading(false);
   };
 
   return (
     <FormContainer>
-      <FormTitle>{ recipeData ? `Edit ${recipeData.title}` :'Create a New Recipe'}</FormTitle>
-      {recipeData && <NavLink onClick={()=>navigate.goBack()} style={{fontWeight:'bolder',display:'block', color:"#F8CC36",marginBottom:'20px'}} to={`/recipe/${recipeData._id}`}>Explore  {'>'} Edit</NavLink>}
+      <FormTitle>
+        {recipeData ? `Edit ${recipeData.title}` : "Create a New Recipe"}
+      </FormTitle>
+      {recipeData && (
+        <NavLink
+          onClick={() => navigate.goBack()}
+          style={{
+            fontWeight: "bolder",
+            display: "block",
+            color: "#F8CC36",
+            marginBottom: "20px",
+          }}
+          to={`/recipe/${recipeData._id}`}
+        >
+          Explore {">"} Edit
+        </NavLink>
+      )}
       <form onSubmit={recipeData ? handleUpdate : handleFormSubmit}>
-        <InputWrapper  >
-        <InputField
-          type="text"
-          value={recipeName}
-          onChange={(e) => setRecipeName(e.target.value)}
-          placeholder="Recipe Name"
-          required
-        />
+        <InputWrapper>
+          <InputField
+            type="text"
+            value={recipeName}
+            onChange={(e) => setRecipeName(e.target.value)}
+            placeholder="Recipe Name"
+            required
+          />
         </InputWrapper>
         <IngredientsContainer>
           {ingredients.map((ingredient, index) => (
-            <div key={index} style={{ display: 'flex',alignContent:'center',alignItems:'center',marginTop:'10px'}}>
+            <div
+              key={index}
+              style={{
+                display: "flex",
+                alignContent: "center",
+                alignItems: "center",
+                marginTop: "10px",
+              }}
+            >
               <InputField
                 type="text"
                 value={ingredient}
@@ -93,7 +131,10 @@ const CreateRecipe = () => {
                 placeholder={`Ingredient ${index + 1}`}
                 required
               />
-              <RemoveButton type="button" onClick={() => removeIngredientField(index)}>
+              <RemoveButton
+                type="button"
+                onClick={() => removeIngredientField(index)}
+              >
                 Remove
               </RemoveButton>
             </div>
@@ -107,12 +148,27 @@ const CreateRecipe = () => {
           onChange={setInstructions}
           placeholder="Enter instructions..."
         />
-           <ImageUploadContainer>
-          <Label htmlFor="image" style={{marginTop:'30px'}}>Upload Image</Label>
-          <Input type="file" id="image" accept="image/*" onChange={handleImageChange} />
-          {imagePreview && <RecipeImage src={imagePreview} alt="Recipe Preview" />}
+        <ImageUploadContainer>
+          <Label htmlFor="image" style={{ marginTop: "30px" }}>
+            Upload Image
+          </Label>
+          <Input
+            type="file"
+            id="image"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+          {imagePreview && (
+            <RecipeImage src={imagePreview} alt="Recipe Preview" />
+          )}
         </ImageUploadContainer>
-        <SubmitButton type="submit">{recipeData ? 'Update Recipe' : 'Submit Recipe'}</SubmitButton>
+        <SubmitButton type="submit">
+          {loading
+            ? "Submitting..."
+            : recipeData
+            ? "Update Recipe"
+            : "Submit Recipe"}
+        </SubmitButton>
       </form>
     </FormContainer>
   );
@@ -181,7 +237,7 @@ const RemoveButton = styled.button`
 const SubmitButton = styled.button`
   width: 100%;
   padding: 10px;
-  background-color: #F8CC36;
+  background-color: #f8cc36;
   border: none;
   border-radius: 4px;
   color: white;
@@ -203,9 +259,8 @@ const RecipeImage = styled.img`
 `;
 
 const InputWrapper = styled.div`
-width:84.5% ;
-@media screen and (max-width:700px) {
-  width: 93%;
-  
-}
-`
+  width: 84.5%;
+  @media screen and (max-width: 700px) {
+    width: 93%;
+  }
+`;
